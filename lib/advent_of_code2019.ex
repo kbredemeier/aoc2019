1cae0d569cef9dc4834b7ffb6f40b7224aee9a92
Kristopher Bredemeier <k.bredemeier@gmail.com>
2019-12-03 18:28:23 +0100

    Adds benchmark/2 to measure execution time

--- lib/advent_of_code2019.ex
+++ lib/advent_of_code2019.ex
@@ -3,4 +3,12 @@ defmodule AdventOfCode2019 do
   This project contains the solutions for the Advent of Code challenges from
   2019.
   """
+
+  def benchmark(fun, unit \\ :millisecond) do
+    start_time = System.monotonic_time(unit)
+    result = fun.()
+    end_time = System.monotonic_time(unit)
+    IO.puts("Took #{end_time - start_time} #{unit}s")
+    result
+  end
 end
