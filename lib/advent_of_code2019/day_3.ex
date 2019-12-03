cfbffd880e29cc1236f97eac937bc0171240ad0d
Kristopher Bredemeier <k.bredemeier@gmail.com>
2019-12-03 17:39:54 +0100

    Refactors fuel management system

--- lib/advent_of_code2019/day_3.ex
+++ lib/advent_of_code2019/day_3.ex
@@ -1,41 +1,50 @@
 defmodule AdventOfCode2019.Day3 do
   def part_1 do
-    [cab1_directions, cab2_directions, _] = read_input_file()
+    [wire1_directions, wire2_directions] = read_input_file()
 
-    cab1_coordinates =
-      [{0, 0}] |> calculate_path(cab1_directions) |> MapSet.new()
+    # Builds up a list with all coordinates that a wire crosses in reverse order
+    # (meaning the first element is where the wire ends)
+    wire1_coordinates =
+      [{0, 0}] |> calculate_path(wire1_directions) |> MapSet.new()
 
-    cab2_coordinates =
-      [{0, 0}] |> calculate_path(cab2_directions) |> MapSet.new()
+    wire2_coordinates =
+      [{0, 0}] |> calculate_path(wire2_directions) |> MapSet.new()
 
-    intersections = MapSet.intersection(cab1_coordinates, cab2_coordinates)
+    intersections = MapSet.intersection(wire1_coordinates, wire2_coordinates)
 
+    # Gets the manhattan distance from the second largest element. The first
+    # element is the distance at {0, 0} which is 0.
     intersections
-    |> Enum.map(&calc_point_distance({0, 0}, &1))
+    |> Enum.map(&calc_manhattan_distance({0, 0}, &1))
     |> Enum.sort()
     |> Enum.at(1)
   end
 
   def part_2 do
-    [cab1_directions, cab2_directions, _] = read_input_file()
+    [wire1_directions, wire2_directions] = read_input_file()
 
-    cab1_coordinates =
-      [{0, 0}] |> calculate_path(cab1_directions) |> Enum.reverse()
+    # To get the actual distance of each coordinate where the wires intersect
+    # the list must be reversed. That way the index is also the distance.
+    wire1_coordinates =
+      [{0, 0}] |> calculate_path(wire1_directions) |> Enum.reverse()
 
-    cab2_coordinates =
-      [{0, 0}] |> calculate_path(cab2_directions) |> Enum.reverse()
+    wire2_coordinates =
+      [{0, 0}] |> calculate_path(wire2_directions) |> Enum.reverse()
 
     intersections =
       MapSet.intersection(
-        MapSet.new(cab1_coordinates),
-        MapSet.new(cab2_coordinates)
+        MapSet.new(wire1_coordinates),
+        MapSet.new(wire2_coordinates)
       )
 
+    # Gets the distance for each wire at the intersections by finding the index
+    # in each wire's list of coordinates. Again, the first element is the
+    # distance at {0, 0}.
     intersections
     |> Enum.map(fn coordinates ->
-      cab1_index = Enum.find_index(cab1_coordinates, &(&1 == coordinates))
-      cab2_index = Enum.find_index(cab2_coordinates, &(&1 == coordinates))
-      cab1_index + cab2_index
+      wire1_distance = Enum.find_index(wire1_coordinates, &(&1 == coordinates))
+      wire2_distance = Enum.find_index(wire2_coordinates, &(&1 == coordinates))
+      wire1_distance + wire2_distance
     end)
     |> Enum.sort()
     |> Enum.at(1)
@@ -44,18 +53,18 @@ defmodule AdventOfCode2019.Day3 do
   def calculate_path(path, []), do: path
 
   def calculate_path(path, [next_instruction | tail]) do
-    line = calc_step_coordinates(next_instruction, path)
+    line = calc_instruction_path(next_instruction, path)
     calculate_path(line, tail)
   end
 
-  def calc_step_coordinates({_, 0}, coordinates), do: coordinates
+  def calc_instruction_path({_, 0}, coordinates), do: coordinates
 
-  def calc_step_coordinates(
-        {dir, dist} = direction,
+  def calc_instruction_path(
+        {dir, dist},
         [last_coordinates | _] = path
       ) do
     step = calc_next_coordinates(last_coordinates, {dir, 1})
-    calc_step_coordinates({dir, dist - 1}, [step | path])
+    calc_instruction_path({dir, dist - 1}, [step | path])
   end
 
   def calc_next_coordinates({x, y}, {:up, dist}), do: {x, y + dist}
@@ -63,7 +72,7 @@ defmodule AdventOfCode2019.Day3 do
   def calc_next_coordinates({x, y}, {:right, dist}), do: {x + dist, y}
   def calc_next_coordinates({x, y}, {:left, dist}), do: {x - dist, y}
 
-  def calc_point_distance({start_x, start_y}, {end_x, end_y}) do
+  def calc_manhattan_distance({start_x, start_y}, {end_x, end_y}) do
     calc_axis_distance(start_x, end_x) + calc_axis_distance(start_y, end_y)
   end
 
@@ -71,30 +80,23 @@ defmodule AdventOfCode2019.Day3 do
   def calc_axis_distance(x, y) when x > y, do: x - y
   def calc_axis_distance(x, y) when y > x, do: y - x
 
+  def parse_instruction("U" <> dist), do: {:up, String.to_integer(dist)}
+  def parse_instruction("D" <> dist), do: {:down, String.to_integer(dist)}
+  def parse_instruction("L" <> dist), do: {:left, String.to_integer(dist)}
+  def parse_instruction("R" <> dist), do: {:right, String.to_integer(dist)}
+
   defp read_input_file do
     :aoc2019
     |> :code.priv_dir()
     |> Path.join("day_3/input.txt")
     |> File.read!()
+    |> String.trim()
     |> String.split("\n")
     |> Enum.map(&String.split(&1, ","))
     |> Enum.map(fn directions ->
       directions
-      |> Enum.reject(fn
-        "" -> true
-        _ -> false
-      end)
+      |> Enum.reject(&(&1 == ""))
       |> Enum.map(&parse_instruction/1)
     end)
-  end
-
-  def parse_instruction("U" <> dist), do: build_directions_tuple(:up, dist)
-  def parse_instruction("D" <> dist), do: build_directions_tuple(:down, dist)
-  def parse_instruction("L" <> dist), do: build_directions_tuple(:left, dist)
-  def parse_instruction("R" <> dist), do: build_directions_tuple(:right, dist)
-
-  defp build_directions_tuple(dir, dist_str) do
-    {dist, _} = Integer.parse(dist_str)
-    {dir, dist}
   end
 end
--- test/day_3_test.exs
+++ test/day_3_test.exs
@@ -3,11 +3,19 @@ defmodule AdventOfCode2019.Day3Test do
 
   alias AdventOfCode2019.Day3, as: Subject
 
+  test "part_1/0" do
+    assert 209 == Subject.part_1()
+  end
+
+  test "part_2/0" do
+    assert 43_258 == Subject.part_2()
+  end
+
   test "parse_instruction/1" do
     assert {:up, 10} = Subject.parse_instruction("U10")
     assert {:down, 10} = Subject.parse_instruction("D10")
-    assert {:left, 10} = Subject.parse_instruction("U10")
-    assert {:right, 10} = Subject.parse_instruction("U10")
+    assert {:left, 10} = Subject.parse_instruction("L10")
+    assert {:right, 10} = Subject.parse_instruction("R10")
   end
 
   test "calc_next_coordinates/2" do
@@ -24,13 +32,13 @@ defmodule AdventOfCode2019.Day3Test do
 
   test "calc_step_coordinats/2" do
     assert [{0, 2}, {0, 1}, {0, 0}] =
-             Subject.calc_step_coordinates({:up, 2}, [{0, 0}])
+             Subject.calc_instruction_path({:up, 2}, [{0, 0}])
 
     assert [{0, 2}, {0, 1}, {0, 0}, {1, 0}] =
-             Subject.calc_step_coordinates({:up, 2}, [{0, 0}, {1, 0}])
+             Subject.calc_instruction_path({:up, 2}, [{0, 0}, {1, 0}])
 
     assert [{1, 2}, {1, 1}, {1, 0}, {0, 0}] =
-             Subject.calc_step_coordinates({:up, 2}, [{1, 0}, {0, 0}])
+             Subject.calc_instruction_path({:up, 2}, [{1, 0}, {0, 0}])
   end
 
   test "calculate_path/2" do
@@ -44,13 +52,13 @@ defmodule AdventOfCode2019.Day3Test do
              Subject.calculate_path([{0, 0}], [{:up, 2}, {:left, 2}])
   end
 
-  test "calc_point_distance/2" do
-    assert 0 == Subject.calc_point_distance({0, 0}, {0, 0})
-    assert 1 == Subject.calc_point_distance({0, 0}, {1, 0})
-    assert 1 == Subject.calc_point_distance({0, 0}, {0, 1})
-    assert 1 == Subject.calc_point_distance({1, 0}, {0, 0})
-    assert 1 == Subject.calc_point_distance({0, 1}, {0, 0})
-    assert 2 == Subject.calc_point_distance({0, 0}, {1, 1})
+  test "calc_manhattan_distance/2" do
+    assert 0 == Subject.calc_manhattan_distance({0, 0}, {0, 0})
+    assert 1 == Subject.calc_manhattan_distance({0, 0}, {1, 0})
+    assert 1 == Subject.calc_manhattan_distance({0, 0}, {0, 1})
+    assert 1 == Subject.calc_manhattan_distance({1, 0}, {0, 0})
+    assert 1 == Subject.calc_manhattan_distance({0, 1}, {0, 0})
+    assert 2 == Subject.calc_manhattan_distance({0, 0}, {1, 1})
   end
 
   test "calc_axis_distance/2" do
